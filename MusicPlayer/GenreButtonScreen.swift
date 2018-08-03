f281774a96f44bb247fa6c36562caa0afda67451
Zwerty <zwerty.il@yandex.ru>
2018-08-03 22:00:05 +0300

    Initial Commit

---
+++ MusicPlayer/GenreButtonScreen.swift
@@ -0,0 +1,30 @@
+//
+//  GenreButtonScreen.swift
+//  MusicPlayer
+//
+//  Created by Дмитрий Гусеница on 29.07.2018.
+//  Copyright © 2018 Дмитрий Гусеница. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+}
